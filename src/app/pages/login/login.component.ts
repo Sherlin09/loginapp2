51d363f09ca147b482ab13f467c89e13edf279a8
Sherlin Toriz Pérez <sherlintp09@gmail.com>
2020-10-29 20:39:35 -0600

    171 tarea

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { UsuarioModel } from '../../models/usuario.model';
+import { AuthService } from '../../serices/auth.service';
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
+
+
+
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +16,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private auth:AuthService,
+              private router: Router) { }
+  
+  usuario: UsuarioModel = new UsuarioModel();
+  recordarme = false;
+
 
   ngOnInit() {
+    if (localStorage.getItem('email')) {
+        this.usuario.email = localStorage.getItem('email');
+        this.recordarme = true;
+    }
+
+  }
+
+  login(form: NgForm){
+
+    
+    if( form.invalid){return;}
+    Swal.fire({
+      allowOutsideClick:false,
+      icon: 'info',
+      text: 'Espere por favor...'
+    });
+  Swal.showLoading();
+    
+
+
+    this.auth.login(this.usuario)
+    .subscribe( resp =>{
+
+      console.log(resp);
+      Swal.close();
+
+    if (this.recordarme) {
+      localStorage.setItem('email', this.usuario.email);
+    }
+
+      this.router.navigateByUrl('/home');
+
+    }, (err) =>{
+        
+      console.log(err.error.error.message);
+      Swal.fire({
+        icon: 'error',
+        title: 'Error al autenticar',
+        text: err.error.error.message
+      });
+
+
+    });
+    
   }
 
 }
